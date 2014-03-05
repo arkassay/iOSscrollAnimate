ce5ad950e6a7a5a3ec6b62b003089d62d421a9c5
akassay <arkassay@gmail.com>
2014-03-05 18:50:03 -0500

    first animations working (on touchend event) need to change to onmove

--- js/core.js
+++ js/core.js
@@ -15,12 +15,14 @@ var pieces = function(){
 var pageLoad = {
 	
 	loading : true,	
+	pieces : [],
 		
 	init : function(){
-		
 		self = this;
-		$( window ).load(function(){
+		
+		$( window ).load( function(){
 			this.loading = false;
+			
 			self.hideLoadingScreen();
 			self.showMenu();
 		});
@@ -28,18 +30,93 @@ var pageLoad = {
 	},
 	
 	hideLoadingScreen : function(){
-		$('.loading').animate({
+		$( '.loading' ).animate({
 			'top' : '-480px',
 		}, 800, 'easeInExpo', function(){
-			$(this).hide();
+			$( this ).hide();
 		});
 	},
 	
 	showMenu : function(){
-		$('.menu').show();
+		//$( '.menu' ).show();
+		//var menHeight = $('.menu').height();
+  	 	//var $menuEl = $( '.menu' );
+		var self = this;
+		var $pieces	= $( '.piece' );
+		
+		$.each( $pieces, function( index, value ){
+			var offset = $( value ).offset().top;
+			self.pieces.push( offset );
+		});
+		
+		$.each($pieces, function( index, value ){
+			var offset = self.pieces[ index ];
+			$( value ).css({
+				'top' : offset,
+				'position' : 'absolute'
+			});
+		});
+		
+  	 	var lastY, change;
+  	 	var pos = 0;
+  	 	var startY = 0;
+  	 	var endY = 0;
+  	 	$( 'body' ).on( 'touchstart', function( e ){
+  	 		console.log( e );
+  	 		startY = e.originalEvent.pageY;
+  	 		console.log("START: " + startY);
+  	 	});
+  	 	$( 'body' ).on( 'touchmove', function( e ){
+  	 		endY = e.originalEvent.targetTouches[0].pageY;
+  	 		
+  	 	} )
+  	 	
+  	 	$( 'body' ).on( 'touchend', function( e ){
+  	 		
+  	 		var currentY = e.originalEvent.pageY;
+  	 		if ( endY > startY && endY != 0) {
+        		//console.log('moving down');
+        		change = endY - startY;
+        		var j = 0;
+        		
+        		for( i = $pieces.length, len = 0; i >= 0; i-- ){
+        			
+        			self.pieces[ i ] = self.pieces[ i ] + change;
+        			$( $pieces[ i ] ).delay( j * 100 * .7 ).animate( {
+        				'top':  self.pieces[ i ]
+        			}, 500 );
+        			j++
+        		}
+    		} else {
+    			if( endY != 0 ){
+    				//console.log('moving up');
+        			change = (startY - endY);
+        			var j = 0;
+        		
+        			for( i = 0, len = $pieces.length; i < len; i++ ){
+            			
+            			self.pieces[ i ] = self.pieces[ i ] - change;
+            			
+            			if( self.pieces[ i ] > -240 ){
+            				$( $pieces[ i ] ).delay( j * 50 * .7 ).animate( {
+                				'top':  self.pieces[ i ]
+                			}, 1000, 'easeOutExpo' );
+            				j++;
+            			}
+            			
+            			
+            			
+            		}
+    			}
+    			
+    		}
+  	 	})
+		
+		
 	}
 	
 	
 }
 
 pageLoad.init();
+
