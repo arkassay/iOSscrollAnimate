15f9c55ba49fb85f7afe9dbd0a3ad2935f8e7c55
akassay <arkassay@gmail.com>
2014-03-06 18:06:47 -0500

    changed overflow to scroll for wrapper

--- js/core.js
+++ js/core.js
@@ -1,21 +1,143 @@
-var pieces = function(){
+var Pieces = function( options ){
+	
+	this.piecesOffsets  = [];
+	this.$pieces = null;
 	
 	this.init = function(){
+		var self = this;
+		this.$pieces	= $( '.piece' );
+		this.$promo = $( '.promo' );
+		//save initial offsets
+		$.each( self.$pieces, function( index, value ){
+			var offset = {
+				init : $( value ).offset().top,
+				current : $( value ).offset().top
+			}
+			self.piecesOffsets.push( offset );
+		});
+		
+		//set css with position absolute and offsets
+		$.each(self.$pieces, function( index, value ){
+			var offset = self.piecesOffsets[ index ].init;
+			$( value ).css({
+				'top' : offset,
+				'position' : 'absolute'
+			});
+		});
+		
+		//this.movePromo();
+		this.handlers();
 		
 	};
 	
-	this.moveMe = function(){
+	this.movePromo = function(){
+		var startY, endY, heightChange, speedVar;
+		var self = this;
+		
+		heightChange = $( self.$promo[ 0 ] ).height();
+		speedVar = 1700;
+		
+		for( i = 0, len = this.$promo.length; i < len; i++ ){
+			setTimeout(function(){
+				pageLoad.a.movePieces( heightChange, 0 );
+			}, (i+1)*speedVar);
+		}
 		
 	};
 	
+	this.movePieces = function( startY, endY ){
+		var self = this;
+		var change;
+		self.$pieces.stop( true );
+		if ( endY > startY && (endY - startY) != 0) {
+    		//console.log('moving down');
+    		change = endY - startY;
+    		var j = 0;
+    		
+    		for( i = self.$pieces.length-1, len = 0; i >= 0; i-- ){
+
+    			if( self.piecesOffsets[ i ].current + change >= self.piecesOffsets[ i ].init ){
+    				self.piecesOffsets[ i ].current = self.piecesOffsets[ i ].init;
+				}else{
+					self.piecesOffsets[ i ].current = self.piecesOffsets[ i ].current + change;
+				}
+    			
+    				$( self.$pieces[ i ] ).delay( j * 30 * .7 ).animate( {
+        				'top':  self.piecesOffsets[ i ].current
+        			}, 2000, 'easeOutQuad' );
+        			j++
+    			
+    		}
+		} else {
+			if( ( startY - endY ) != 0 ){
+				//console.log('moving up');
+    			change = (startY - endY);
+    			var j = 0;
+    		
+    			for( i = 0, len = self.$pieces.length; i < len; i++ ){
+        			if( self.piecesOffsets[ i ].init + endY > -$( '.menu' ).position().top ){
+        				//animate the pieces that are in frame
+        				self.piecesOffsets[ i ].current = self.piecesOffsets[ i ].current - change;
+        				$( self.$pieces[ i ] ).delay( j * 40 * .7 ).animate( {
+            				'top':  self.piecesOffsets[ i ].current 
+            			}, j * 200 * .6, 'easeOutQuad' );
+        				j++;
+        			}else{
+        				//if the piece is out of frame, save it's new offset in the array but don't animate
+        				self.piecesOffsets[ i ].current = self.piecesOffsets[ i ].current - change;
+        			}
+        		}
+    			
+    			console.log( j );
+    			//console.log( self.piecesOffsets )
+			}
+			
+		}
+		//endY = startY;
+	};
 	
+	this.handlers = function(){
+		var self = this;
+		var startY, endY, lastY, currY;
+		var to;
+  	 	
+  	 	var endY = 0;
+  	 	$( '.menu' ).on( 'touchstart', function( e ){
+  	 		//console.log( e );
+  	 		startY = e.originalEvent.pageY;
+  	 		
+  	 		//reset the endY so on touch without move, there is no delta
+  	 		endY = startY;
+  	 	});
+  	 	
+  	 	
+  	 	$( '.menu' ).on( 'touchmove', function( e ){	
+  	 		
+  	 		currY = e.originalEvent.targetTouches[0].pageY;
+  	 		
+  	 		
+  	 		console.log( "BEFORE: " + lastY );
+  	 		console.log( "AFTER: " + currY)
+  	 
+  	 		
+  	 		endY = currY;
+
+  	 	});
+  	 	
+  	 	$( '.menu' ).on( 'touchend', function( e ){
+  	 		//var currentY = e.originalEvent.pageY;
+  	 		self.movePieces( startY, endY );
+  	 	})
+		
+		
+	}
 };
 
 
 var pageLoad = {
 	
 	loading : true,	
-	pieces : [],
+	a : null,
 		
 	init : function(){
 		self = this;
@@ -32,86 +154,15 @@ var pageLoad = {
 	hideLoadingScreen : function(){
 		$( '.loading' ).animate({
 			'top' : '-480px',
-		}, 800, 'easeInExpo', function(){
+		}, 1000, 'easeInExpo', function(){
 			$( this ).hide();
 		});
 	},
 	
 	showMenu : function(){
-		//$( '.menu' ).show();
-		//var menHeight = $('.menu').height();
-  	 	//var $menuEl = $( '.menu' );
-		var self = this;
-		var $pieces	= $( '.piece' );
 		
-		$.each( $pieces, function( index, value ){
-			var offset = $( value ).offset().top;
-			self.pieces.push( offset );
-		});
-		
-		$.each($pieces, function( index, value ){
-			var offset = self.pieces[ index ];
-			$( value ).css({
-				'top' : offset,
-				'position' : 'absolute'
-			});
-		});
-		
-  	 	var lastY, change;
-  	 	var pos = 0;
-  	 	var startY = 0;
-  	 	var endY = 0;
-  	 	$( 'body' ).on( 'touchstart', function( e ){
-  	 		console.log( e );
-  	 		startY = e.originalEvent.pageY;
-  	 		console.log("START: " + startY);
-  	 	});
-  	 	$( 'body' ).on( 'touchmove', function( e ){
-  	 		endY = e.originalEvent.targetTouches[0].pageY;
-  	 		
-  	 	} )
-  	 	
-  	 	$( 'body' ).on( 'touchend', function( e ){
-  	 		
-  	 		var currentY = e.originalEvent.pageY;
-  	 		if ( endY > startY && endY != 0) {
-        		//console.log('moving down');
-        		change = endY - startY;
-        		var j = 0;
-        		
-        		for( i = $pieces.length, len = 0; i >= 0; i-- ){
-        			
-        			self.pieces[ i ] = self.pieces[ i ] + change;
-        			$( $pieces[ i ] ).delay( j * 100 * .7 ).animate( {
-        				'top':  self.pieces[ i ]
-        			}, 500 );
-        			j++
-        		}
-    		} else {
-    			if( endY != 0 ){
-    				//console.log('moving up');
-        			change = (startY - endY);
-        			var j = 0;
-        		
-        			for( i = 0, len = $pieces.length; i < len; i++ ){
-            			
-            			self.pieces[ i ] = self.pieces[ i ] - change;
-            			
-            			if( self.pieces[ i ] > -240 ){
-            				$( $pieces[ i ] ).delay( j * 50 * .7 ).animate( {
-                				'top':  self.pieces[ i ]
-                			}, 1000, 'easeOutExpo' );
-            				j++;
-            			}
-            			
-            			
-            			
-            		}
-    			}
-    			
-    		}
-  	 	})
-		
+		this.a = new Pieces();
+		this.a.init();
 		
 	}
 	
